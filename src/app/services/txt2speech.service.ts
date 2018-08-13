3393bc89fb5de274dd40b7ebe682fff088c1b839
offlineprogrammer <mmalaka@gmail.com>
2018-08-12 22:22:26 -0700

    API

--- src/app/services/txt2speech.service.ts
+++ src/app/services/txt2speech.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Txt2speechService {
+  txt2SpeechAPIUrl: string = 'https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=';
 
-  constructor() { }
+ 
+
+  constructor(private http: HttpClient) { }
+
+  getWordDef(word:string) {
+    return new Promise(resolve => {
+      this.http.post(this.txt2SpeechAPIUrl+word).subscribe(data => {
+        
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+
 }
